e3288e00ffb804e89c4a8806bdfbb6ed19574d24
Sebastian Jones <83433069+SJones14@users.noreply.github.com>
2023-02-21 12:42:37 -0500

    Add files via upload

---
+++ Pages/AddSchool.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Data.SqlClient;
+using System.Data.Common;
+
+namespace test.Pages
+{
+
+    public class AddSchoolModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public AddSchoolModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+
+    }
+}
