b9f66d7a69fcc253f40a2fa3d0dd521d32aa25f2
Orry Willems <willemso@findlay.edu>
2023-03-14 11:07:55 -0400

    z

---
+++ WebApplication4/WebApplication4 - Copy/WebApplication4/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication4.Pages;
+
+namespace WebApplication4.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index(CountryModel country)
+        {
+            country.CountryList = CountryDate();
+            if(country.CountryList != null)
+            {
+                List<SelectListItem> selectedItems = country.CountryList.Where(p => country.CountryId.Contains(int.Parse(p.Value))).ToList();
+                ViewBag.Message = "Selected Countries:";
+                foreach (var selectedItem in selectedItems)
+                {
+                    selectedItem.Selected = true;
+                    ViewBag.Message += "\\n" + selectedItem.Text;
+                }
+            }
+            
+            
+            return View(country);
+        }
+        public List<SelectListItem> CountryDate()
+        {
+            List<SelectListItem> countryItems = new List<SelectListItem>();
+            //objcountry.Add(new Country { CountryName = "America" });
+            // objcountry.Add(new Country { CountryName = "India" });
+            //objcountry.Add(new Country { CountryName = "Sri Lanka" });
+            //objcountry.Add(new Country { CountryName = "China" });
+            //objcountry.Add(new Country { CountryName = "Pakistan" });
+            countryItems.Add(new SelectListItem
+            {
+                Text = "America", //sdr["continentName"].ToString(),
+                Value = "America"//sdr["continentId"].ToString()
+            });
+            countryItems.Add(new SelectListItem
+            {
+                Text = "Europe", //sdr["continentName"].ToString(),
+                Value = "Europe"//sdr["continentId"].ToString()
+            });
+            return countryItems;
+        }
+    }
+}
---
+++ WebApplication4/WebApplication4 - Copy/WebApplication4/Pages/Country.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.Mvc;
+namespace WebApplication4.Pages
+{
+    public class CountryModel
+    {
+        public List<SelectListItem> CountryList { get; set; } 
+        public int[] CountryId { get; set; }
+
+    }
+}
