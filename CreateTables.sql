7b4891f8963a5b94fc05739e97818873d9e0fe5f
willemso <124073320+willemso@users.noreply.github.com>
2023-03-16 12:42:57 -0400

    Update CreateTables.sql

--- CreateTables.sql
+++ CreateTables.sql
@@ -1,20 +1,20 @@
 CREATE TABLE ADMINS(
-	Admin_id INT Primary Key,
+	Admin_id INT IDENTITY(1,1) Primary Key,
 	UserName char(100) NOT NULL,
 	Password char (128) NOT NULL,
 	Salt char(20) NOT NULL
 );
 CREATE TABLE PROGRAMS(
-	Program_ID INT PRIMARY KEY,
+	Program_ID INT IDENTITY(1,1) PRIMARY KEY,
 	Major char(25) NOT NULL
 );
 CREATE TABLE UNIVERSITIES(
-	University_ID INT PRIMARY KEY,
+	University_ID INT IDENTITY(1,1) PRIMARY KEY,
 	Country char(60) NOT NULL,
 	Continent char(15) NOT NULL
 );
 CREATE TABLE HOUSING_TYPE(
-	HousingType_ID INT PRIMARY KEY,
+	HousingType_ID INT IDENTITY(1,1) PRIMARY KEY,
 	HousingType char(50) NOT NULL,
 	HousingDescription char(200) NOT NULL
 );
