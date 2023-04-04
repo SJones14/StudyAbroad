ba21974b94744fb72ca0a095452ff649d41cbad7
Orry Willems <willemso@findlay.edu>
2023-04-04 12:47:03 -0400

    check Unique

--- StudyAbroadMain/test/Pages/AddUniversity.cshtml.cs
+++ StudyAbroadMain/test/Pages/AddUniversity.cshtml.cs
@@ -21,9 +21,19 @@ namespace test.Pages
 
         public RedirectToPageResult OnPost(string uniName, string ctry, string cont)
         {
-            InsertStatus += "University was added";
-            AddSchool.Add(uniName, ctry, cont);
-            return RedirectToPage("/AddProgram");
+            if (AddSchool.checkUnique(uniName, ctry))
+            {
+                InsertStatus += "University was added";
+                AddSchool.Add(uniName, ctry, cont);
+                return RedirectToPage("/AddProgram");
+            }
+            else
+            {
+                InsertStatus += "This University already Exists";
+                return RedirectToPage("/AddProgram");
+            }
+            
+            
         }
     }
 
@@ -31,13 +41,51 @@ namespace test.Pages
     {
         public static void Add(string name, string ctry, string cont)
         {
+            
+            
             SqlConnection dbconnection = new("Server=tcp:ufstudyabroadserver.database.windows.net,1433;Initial Catalog=ufstudyabroadDB;Persist Security Info=False;User ID=tech;Password=StudyAbroad23;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
             SqlDataAdapter adapter = new();
             dbconnection.Open();
-            string sql = "INSERT INTO UNIVERSITIES VALUES (\'" + name + "\', \'" + ctry + "\', \'" + cont + "\');";
-            adapter.InsertCommand = new SqlCommand(sql, dbconnection);
-            adapter.InsertCommand.ExecuteNonQuery();
+
+            
+                string sql = "INSERT INTO UNIVERSITIES VALUES (\'" + name + "\', \'" + ctry + "\', \'" + cont + "\');";
+                adapter.InsertCommand = new SqlCommand(sql, dbconnection);
+                adapter.InsertCommand.ExecuteNonQuery();
+            
+            
             dbconnection.Close();
         }
-    }
+        public static bool checkUnique(string name, string ctry){
+            
+            bool Unique = true;
+
+            SqlConnection dbconnection = new("Server=tcp:ufstudyabroadserver.database.windows.net,1433;Initial Catalog=ufstudyabroadDB;Persist Security Info=False;User ID=tech;Password=StudyAbroad23;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+            dbconnection.Open();
+            SqlCommand command;
+            SqlDataReader reader;
+            string sql = "SELECT University_Name, Country FROM UNIVERSITIES";
+            command = new SqlCommand(sql, dbconnection);
+            reader = command.ExecuteReader();
+            List<string> dbNames = new();
+            List<string> dbCountries = new();
+
+            while (reader.Read())
+            {
+                dbNames.Add(reader.GetValue(0).ToString());
+                dbCountries.Add(reader.GetValue(1).ToString());
+            }
+
+            for (int i = 0; i < dbNames.Count; i++)
+            {
+                if (name == dbNames[i].Trim() && ctry == dbCountries[i].Trim())
+                {
+                    Unique = false;
+                }
+            }
+            dbconnection.Close();
+
+            
+            return Unique;
+        }
+}
 }
