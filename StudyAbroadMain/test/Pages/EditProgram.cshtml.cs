2a086f1d626c8f30d2a8bb37c4da49e7e9234cd7
Orry Willems <willemso@findlay.edu>
2023-04-13 11:41:45 -0400

    J

---
+++ StudyAbroadMain/test/Pages/EditProgram.cshtml.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Data.SqlClient;
+using System.Data.Common;
+
+namespace test.Pages
+{
+
+    public class EditProgramModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public EditProgramModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+
+        public string Result { get; set; }
+
+        public RedirectToPageResult OnPost(string school, string major)
+        {
+            Result += "Program was added!";
+            for (int i = 0; i < school.Length; i++)
+            {
+                if (school[i] == '~')
+                {
+                    school = school.Substring(0, i);
+                }
+            }
+            EditPrograms.AddProgram(school, major);
+            return RedirectToPage("/AdminOptions");
+        }
+
+    }
+
+}
